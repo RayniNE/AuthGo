7fbfc0c95f47fdc23aca0a7b5808b8d6f4736bc0
RayniNE <timetopwm@gmail.com>
2021-08-02 21:01:34 -0400

    Finished the implementation of the the handlers with each endpoint. Created a Product struct, and an array of Products which will be served to the user by its endpoint.

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	_ "encoding/json"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type Product struct {
+	Id          int
+	Name        string
+	Slug        string
+	Description string
+}
+
+var products = []Product{
+	{Id: 1, Name: "World of Authcraft", Slug: "world-of-authcraft", Description: "Battle bugs and protect yourself from invaders while you explore a scary world with no security"},
+	{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
+	{Id: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
+	{Id: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+	{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
+	{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
+}
 
 func main() {
 
@@ -13,9 +29,9 @@ func main() {
 
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 
-	r.Handle("/status", NotImplemented).Methods("GET")
-	r.Handle("/products", NotImplemented).Methods("GET")
-	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+	r.Handle("/status", StatusHandler).Methods("GET")
+	r.Handle("/products", ProductsHandler).Methods("GET")
+	r.Handle("/products/{slug}/feedback", AddFeedBackHandler).Methods("POST")
 
 	http.ListenAndServe(":8000", r)
 
@@ -23,4 +39,43 @@ func main() {
 
 var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not Implemented"))
+})
+
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API is up and running"))
+})
+
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	payload, _ := json.Marshal(products)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+})
+
+var AddFeedBackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	var product Product
+
+	vars := mux.Vars(r)
+
+	slug := vars["slug"]
+
+	for _, p := range products {
+		if p.Slug == slug {
+			product = p
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if product.Slug != "" {
+
+		payload, _ := json.Marshal(product)
+
+		w.Write([]byte(payload))
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+
 })
